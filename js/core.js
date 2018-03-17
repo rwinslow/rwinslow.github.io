bbd000ba4f2503b6674e075a899115acf7ab93a2
Rich Winslow <rwins4@gmail.com>
2018-03-17 01:29:48 -0700

    hash for choosing first page

--- js/core.js
+++ js/core.js
@@ -12,6 +12,24 @@ $(document).ready(function(){
         };
     }
 
+    // Show page if user goes straight to a link with hash.
+    if (window.location.hash) {
+        var hash = window.location.hash.substr(1);
+        $('#page-content-wrapper > .container-fluid > div.' + hash).show();
+        $('.toggle>a.' + hash).addClass('on');
+    }
+
+    // Show page from hash as user moves forward and backward.
+    $(window).on('hashchange', function() {
+        $('.sidebar-nav li a').removeClass('on');
+        $('#page-content-wrapper > .container-fluid > div').hide();
+
+        var hash = window.location.hash.substr(1);
+        $('#page-content-wrapper > .container-fluid > div.' + hash).show();
+        $('.toggle>a.' + hash).addClass('on');
+    });
+
+    // Toggle links and pages on click.
     $('.toggle>a').click(function() {
         var classname = $(this).attr('class');
         $('.sidebar-nav li a').removeClass('on');
