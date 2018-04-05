4158fc2ce617dd8b0de28682153c2b6bf55d13f2
Rich Winslow <rwins4@gmail.com>
2018-04-04 23:23:02 -0700

    add weapons ref

---
+++ libraries/coriolis-weapons.js
@@ -0,0 +1,11 @@
+$('.to-coriolis-ranged').click(function() {
+    $(document).scrollTop( $('.coriolis-ranged').offset().top );
+});
+
+$('.to-coriolis-melee').click(function() {
+    $(document).scrollTop( $('.coriolis-melee').offset().top );
+});
+
+$('.to-coriolis-explosives').click(function() {
+    $(document).scrollTop( $('.coriolis-explosives').offset().top );
+});
