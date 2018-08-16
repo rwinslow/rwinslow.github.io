65dd0b075e2f9680da5bc948135ea0e391d9afb3
Rich Winslow <rwins4@gmail.com>
2018-08-15 22:37:44 -0700

    coriolis: add gear

---
+++ libraries/coriolis-gear.js
@@ -0,0 +1,27 @@
+$('.to-coriolis-living-expenses').click(function() {
+    $(document).scrollTop( $('.coriolis-living-expenses').offset().top );
+});
+$('.to-coriolis-food-drink-entertainment').click(function() {
+    $(document).scrollTop( $('.coriolis-food-drink-entertainment').offset().top );
+});
+$('.to-coriolis-everyday-items').click(function() {
+    $(document).scrollTop( $('.coriolis-everyday-items').offset().top );
+});
+$('.to-coriolis-medicurgical-technology').click(function() {
+    $(document).scrollTop( $('.coriolis-medicurgical-technology').offset().top );
+});
+$('.to-coriolis-tools-repairs').click(function() {
+    $(document).scrollTop( $('.coriolis-tools-repairs').offset().top );
+});
+$('.to-coriolis-survival-colonization').click(function() {
+    $(document).scrollTop( $('.coriolis-survival-colonization').offset().top );
+});
+$('.to-coriolis-exos-vehicles').click(function() {
+    $(document).scrollTop( $('.coriolis-exos-vehicles').offset().top );
+});
+$('.to-coriolis-recon-infiltration').click(function() {
+    $(document).scrollTop( $('.coriolis-recon-infiltration').offset().top );
+});
+$('.to-coriolis-combat-gear').click(function() {
+    $(document).scrollTop( $('.coriolis-combat-gear').offset().top );
+});
